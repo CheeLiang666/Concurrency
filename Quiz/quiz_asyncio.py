8a53e587c5ce7070d85c49ba67290394083a23a6
CheeLiang666 <ccheeliang@gmail.com>
2019-04-01 16:45:17 +0800

    feat: optimize the code and increase the throughput by reducing the time open a file for writing data.

--- Quiz/quiz_asyncio.py
+++ Quiz/quiz_asyncio.py
@@ -1,43 +1,58 @@
 import asyncio
 import time
+import uvloop
 
-async def save_result(file_name, result):
-    with open(file_name, 'a') as f:
-        f.write(result + '\n')
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-
+def save_result(file_name, result):
+    with open(file_name, 'w') as f:
+        for r in result:
+            f.write(r + '\n')
+       
 async def is_palindrome(queue):
-    while True:
+    positive_result = []
+    negative_result = []
+
+    while not queue.empty():
         str_value = await queue.get()
         reverse_str = str_value[::-1]
-        
         if str_value.lower() == reverse_str.lower():
-            await save_result('positive_asyncio.txt', str_value)
+            positive_result.append(str_value)
         else:
-            await save_result('negative_asyncio.txt', str_value)
+            negative_result.append(str_value)
         # Notify the queue that the 'work item' has been processed.
         queue.task_done()
+    return positive_result, negative_result
 
 async def find_palindrome():
     with open('words.txt', 'r') as f:
         data = f.readlines()
         queue = asyncio.Queue()
-        tasks = []
         for s in data:
             queue.put_nowait(s.strip())
+            
+        tasks = []
+        i = 0
+        while i < 5:
             task = asyncio.ensure_future(is_palindrome(queue))
             tasks.append(task)
+            i+=1
+            
         # Wait until the queue is fully processed.
         start_time = time.time()
         await queue.join()
         duration = time.time() - start_time
         print(f'Duration {duration} seconds.')
-        # Cancel the tasks.
-        for task in tasks:
-            task.cancel()
         # Wait until all tasks are cancelled.
-        await asyncio.gather(*tasks, return_exceptions=True)
-
+        result = await asyncio.gather(*tasks, return_exceptions=True)
+        positive_result = []
+        negative_result = []
+        for r in result:
+            positive, negative = r
+            positive_result.extend(positive)
+            negative_result.extend(negative)
+        save_result('positive.txt', positive_result)
+        save_result('negative.txt', negative_result)
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(find_palindrome())
